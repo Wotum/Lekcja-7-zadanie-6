44fec865a3fa513a9ecd3ac07cf6c4dd65d07673
HARDPC <HARDPC@WOTUM>
2024-01-10 17:18:09 +0100

    Add project files.

---
+++ Lekcja 7 zadanie 6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ZmyslonaKategoria;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Podaj swój wzrost (w centymetrach): ");
+
+        if (int.TryParse(Console.ReadLine(), out int wzrost))
+        {
+            string kategoria = (wzrost < 150) ? "Jesteś krasnoludem" : (wzrost < 170) ? "Jesteś średniego wzrostu" : "Jesteś wysoki";
+            Console.WriteLine($"Twój wzrost to {wzrost} cm. {kategoria}");
+        }
+        else
+        {
+            Console.WriteLine("Niepoprawny format danych. Wzrost powinien być liczbą całkowitą.");
+        }
+    }
+}
